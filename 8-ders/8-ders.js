1357fe51894d31f7162190e88bf205cc550b8228
ismailabay <ismailabay.dev@gmail.com>
2022-07-22 16:59:39 +0300

    veri türü işlemleri

---
+++ 8-ders/8-ders.js
@@ -0,0 +1,21 @@
+//String Veri Turu Işlemleri
+//https://www.w3schools.com/jsref/jsref_obj_string.asp
+
+let email = "ismailabay@gmail.com"
+let firstName = "ismail"
+let lastName = "ABAY"
+
+//string karakter sayısı ->lenght
+console.log(email.length)
+console.log(lastName [0])
+console.log(firstName [0])
+
+firstName = firstName.toUpperCase()
+console.log(firstName)
+
+firstName = firstName.toLowerCase()
+console.log(firstName)
+
+console.log(email.search("@"))
+console.log(email[15])
+
