1692042f354932b55957de2fdae6be7897d0bd93
ismailabay <ismailabay.dev@gmail.com>
2022-07-22 17:18:07 +0300

    documant object model

---
+++ 9-ders/9-ders.js
@@ -0,0 +1,10 @@
+//DOM
+//https://developer.mozilla.org/en-US/docs/web/API/Document_Object_Model
+
+//pathname
+console.log(document.location.pathname)
+
+console.log(document.heady)
+console.log(document.body)
+
+document.body.style.backgroundColor = "aqua"
