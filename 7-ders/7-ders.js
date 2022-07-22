9e93eabf35c8e8a9b7589ae672c1cb5bda2f8fc6
ismailabay <ismailabay.dev@gmail.com>
2022-07-22 12:38:40 +0300

    değişken türünü kontrol etmek değişken türünü değiştirmek

---
+++ 7-ders/7-ders.js
@@ -0,0 +1,15 @@
+// template literals kullanımı
+//https://developer.mozilla.org/en-US/docs/Web/javaScript/Reference/Template_Literals
+
+let username = "ismail"
+const DOMAIN = "kodluyoruz.org"
+
+let email = username + "@" + DOMAIN
+console.log("merhaba", username, "sitemize hoşgeldin", "mail adresin:", email )
+
+let info = `merhaba ${username} sitemize hoşgeldin.. mail adresin ${email} 
+mail adresinin uzunluğu:${email.length}
+borcunuz ${(2 + 3) * 10 } TL
+gunun saat bilgisi ${new Date().getHours() }
+kısa isminiz:${username[0]}.`
+console.log(info)
