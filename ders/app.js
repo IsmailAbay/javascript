b376511bd1bca66ccbd34afb75976f078778eaa9
ismailabay <ismailabay.dev@gmail.com>
2022-07-23 23:25:10 +0300

    listeye eleman ekleme

---
+++ ders/app.js
@@ -0,0 +1,12 @@
+let firstItem=document.querySelector('ul>li:first-child')
+let lastItem=document.querySelector('ul>li:last-child')
+
+let ul=document.querySelector('ul')
+
+
+
+
+let li = document.createElement('li')
+li.textContent="MANCHESTER UNITED"
+
+ul.append(li)
