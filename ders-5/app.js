622e94c8fa44c9c87ec0a3e243fd3371a86f3eed
ismailabay <ismailabay.dev@gmail.com>
2022-07-25 13:49:23 +0300

    düzeltme

--- ders-5/app.js
+++ ders-5/app.js
@@ -1,4 +1,3 @@
 let userName = prompt("kullanıcı bilginizi yazınız")
 let info = document.querySelector("#info")
-
 info.İnnerHTML = `${userName.length > 0 ? userName :"kullanıcı bilginiz bulunamadı :( "}`
