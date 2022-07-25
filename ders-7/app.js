2009e178e95e93db36f2bb51924774ad2e18f850
ismailabay <ismailabay.dev@gmail.com>
2022-07-25 16:00:52 +0300

    Fonksiyon Nedir? Neden Kullanırız?

---
+++ ders-7/app.js
@@ -0,0 +1,37 @@
+function helloWorld(){
+    console.log("helloWorld")
+}
+function hello(){
+    console.log("merhaba")
+    helloWorld()
+}
+
+
+
+//hata alabiliriz...
+// if (username && age>=18){
+//     console.log("ehliyet alabilirsiniz.")
+// }
+
+// else if (!
+//     username) {
+//     console.log("kullanıcı adınız yok.")
+// }
+// else if (!(age>=18)){
+//     console.log("kullanıcı adınız yok veya 18 yaşından küçüksünüz.")  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+hello() //calistir
