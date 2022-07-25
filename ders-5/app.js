efc47deb4ee91bc215084c0e2d4cb0b897de7e59
ismailabay <ismailabay.dev@gmail.com>
2022-07-25 12:20:04 +0300

    koşul kullanımı

---
+++ ders-5/app.js
@@ -0,0 +1,4 @@
+let userName = prompt("kullanıcı bilginizi yazınız")
+let info = document.querySelector("#info")
+
+info.İnnerHTML = `${userName.length > 0 ? userName :"kullanıcı bilginiz bulunamadı :( "}`
