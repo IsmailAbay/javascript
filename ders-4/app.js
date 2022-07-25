2dcf0ac7b15c21b7f7b77cb325e6c40da404e5a3
ismailabay <ismailabay.dev@gmail.com>
2022-07-25 11:45:24 +0300

    Çoklu Koşul Yapısı Kullanımı

---
+++ ders-4/app.js
@@ -0,0 +1,25 @@
+let username = prompt("kullanici adiniz")
+let age = prompt("yaşınız")
+
+
+if(!username)
+console.log("adınızı girmediniz")
+
+if(!age)
+console.log("yaşınızı girmediniz")
+
+
+
+// if (username && age>=18){
+//     console.log("ehliyet alabilirsiniz.")
+// }
+
+// else if (!
+//     username) {
+//     console.log("kullanıcı adınız yok.")
+// }
+// else if (!(age>=18)){
+//     console.log("kullanıcı adınız yok veya 18 yaşından küçüksünüz.")  
+// }
+
+
