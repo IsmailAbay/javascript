60fd04f6792db0c14d0d342494ac19d3eca9d3c1
ismailabay <ismailabay.dev@gmail.com>
2022-07-26 16:18:05 +0300

    Fat Arrow Fonksiyonu Kullanımı

---
+++ ders-8/app.js
@@ -0,0 +1,22 @@
+function hello(firstName) {
+    console.log(`merhaba ${"firstName"}`)
+}
+hello("javaScript")
+
+const helloFuncV1 = (firstName) => {console.log(`merhaba ${firstName}`)}
+helloFuncV1("helloFuncV1")
+
+const helloFuncV2 = firstName =>  {console.log(`merhaba ${firstName}`)}
+helloFuncV2("helloFuncV2")
+
+const helloFuncV3 = firstName =>  {console.log(`merhaba ${firstName}`)}
+helloFuncV3("helloFuncV3")
+
+const helloFuncV4 = firstName =>  {console.log(`merhaba ${firstName}`)}
+helloFuncV4("helloFuncV4")
+
+const helloFuncV5 = firstName =>  {console.log(`merhaba ${firstName}`)}
+helloFuncV5("helloFuncV5")
+
+const helloFuncV6 = firstName =>  {console.log(`merhaba ${firstName}`)}
+helloFuncV6("helloFuncV6")
