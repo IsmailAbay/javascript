766d507b0d10fed1b1c84cfc222db219922e50b4
ismailabay <ismailabay.dev@gmail.com>
2022-07-27 14:25:43 +0300

    Input İçerisinden Değer Almak

---
+++ 27-ders/27-ders.js
@@ -0,0 +1,9 @@
+let formDOM = document.querySelector("#userForm")
+formDOM.addEventListener("sumbit",formSumbit)
+
+function formSumbit(event) {
+    event.preventDefault()
+    console.log("islem gerçeklesti")
+    let scoreInputDOM = document.querySelector("#score")
+    console.log(scoreInputDOM.value)
+}
