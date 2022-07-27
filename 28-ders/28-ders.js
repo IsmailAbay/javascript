d85b12684245e969f277c83c773dc7f6868382c9
ismailabay <ismailabay.dev@gmail.com>
2022-07-27 15:02:20 +0300

    Bölüm Sonu Egzersizi

---
+++ 28-ders/28-ders.js
@@ -0,0 +1,13 @@
+let userFormDOM = document.querySelector("#userForm")
+userFormDOM.addEventListener("sumbit",formHandler)
+
+function formHandler(event) {
+    event.preventDefault()
+    const USER_NAME = document.querySelector("#username")
+    const SCORE = document.querySelector("#score")
+}
+
+
+
+function addItem(userName,)
+
