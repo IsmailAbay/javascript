d39fb3128dcb270ff69c17846112821d34cd3042
ismailabay <ismailabay.dev@gmail.com>
2022-07-27 13:15:58 +0300

    localStorage içerisine Farklı Türde Veriler Ekleme

---
+++ 24-ders/24-ders.js
@@ -0,0 +1,8 @@
+
+let user = {userName: "ismail abay", isActive:true}
+
+localStorage.setItem("userInfo",JSON.stringify(user))
+
+let userInfo = localStorage.getItem("userInfo")
+userInfo = JSON.parse(userInfo)
+console.log(userInfo)
