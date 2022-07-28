001d24ec12e75ce5342d0811e852caa4d1f7987a
ismailabay <ismailabay.dev@gmail.com>
2022-07-28 15:46:30 +0300

    22-ders

--- 22-ders/22-ders.js
+++ 22-ders/22-ders.js
@@ -1,4 +1,4 @@
-let counter =0
+let counter = 0
 let counterDOM = document.querySelector("#counter")
 let increaseDOM = document.querySelector("#increase")
 let decreaseDOM = document.querySelector("#decrease")
